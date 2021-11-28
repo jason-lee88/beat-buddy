099181321ce86f5b1822a8f6a729f0f6b69cb35b
Jason Lee <jason.e.lee8@gmail.com>
2021-11-28 02:10:19 -0500

    navbar.js created for modularization

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,22 +1,3 @@
-window.onload = function() {
-    const home = document.getElementById("home");
-    if (home) {
-        home.addEventListener("click", (e) => {
-            location.href = '/';
-        });
-    }
+window.addEventListener('load', function() {
 
-    const users = document.getElementById("users");
-    if (users) {
-        users.addEventListener("click", (e) => {
-            location.href = '/users';
-        });
-    }
-
-    const profile = document.getElementById("profile");
-    if (profile) {
-        profile.addEventListener("click", (e) => {
-            location.href = '/profile';
-        })
-    }
-}
+});
