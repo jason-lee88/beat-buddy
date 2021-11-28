b57abf3ca14a20af96bd77be151874fe49657691
Jason Lee <jason.e.lee8@gmail.com>
2021-11-28 01:57:52 -0500

    Navbar and page wireframes added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Beat Buddy', username: 'JuiceSpoon'});
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('users', { title: 'Search Users', username: 'JuiceSpoon'});
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'My Profile', username: 'JuiceSpoon'})
 });
 
 module.exports = router;
