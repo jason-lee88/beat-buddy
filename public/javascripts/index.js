1c5e1fead308bb9fefc8386cbd5b474155dcd614
Jason Lee <jason.e.lee8@gmail.com>
2021-11-28 02:57:27 -0500

    Sign up, sign in, profile, and user divs all added. All pages styled

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', function() {
+    const signup = document.getElementById("signup");
+    if (signup) {
+        signup.addEventListener("click", (e) => {
+            location.href = '/signup';
+        });
+    }
 
+    const signin = document.getElementById("signin");
+    if (signin) {
+        signin.addEventListener("click", (e) => {
+            location.href = '/signin';
+        });
+    }
 });
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Beat Buddy', username: 'JuiceSpoon'});
+  res.render('index', { title: 'Beat Buddy', username: 'JsonDerulo'});
 });
 
 router.get('/users', function(req, res, next) {
-  res.render('users', { title: 'Search Users', username: 'JuiceSpoon'});
+  res.render('users', { title: 'Search Users', username: 'JsonDerulo'});
 });
 
 router.get('/profile', function(req, res, next) {
-  res.render('profile', { title: 'My Profile', username: 'JuiceSpoon'})
+  res.render('profile', { title: 'My Profile', username: 'JsonDerulo'})
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Create an Account' })
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { title: 'Sign In' })
 });
 
 module.exports = router;
