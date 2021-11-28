fd6fc4454e7b1f5859cf4f9c22ca9bfa348e4e35
Jason Lee <jason.e.lee8@gmail.com>
2021-11-28 01:30:00 -0500

    Home Page Done! (placeholder for concert card is currently there)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Beat Buddy' });
+  res.render('index', { title: 'Beat Buddy', username: 'JuiceSpoon'});
 });
 
 module.exports = router;
